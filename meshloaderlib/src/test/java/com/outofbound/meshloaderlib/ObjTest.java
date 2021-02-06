fb3ffd4b764c048243da51db85ff746a48d82d74
MarcoCiaramella <marcociaramella87@gmail.com>
2021-02-06 13:50:02 +0100

    refactoring. Added junit test.

--- meshloaderlib/src/test/java/com/outofbound/meshloaderlib/ObjTest.java
+++ meshloaderlib/src/test/java/com/outofbound/meshloaderlib/ObjTest.java
@@ -1,10 +1,19 @@
 package com.outofbound.meshloaderlib;
 
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import com.outofbound.meshloaderlib.obj.Obj;
+
 import org.junit.Test;
 
 public class ObjTest {
 
     @Test
     public void load_isCorrect(){
+        Context context = ApplicationProvider.getApplicationContext();
+        Obj obj = new Obj(context,"cube.obj");
+        obj.load();
     }
 }
